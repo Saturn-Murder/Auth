e3bf9a27e9c48be5670d8c8322d0f63becc5a51b
Kirill <Kirillica2008@outlook.com>
2023-11-12 20:10:48 +0300

    кирилл двоечник

--- src/App.tsx
+++ src/App.tsx
@@ -3,11 +3,16 @@ import { CustomLabel } from "./CustomLabel"
 
 function App() {
 
+      
+
   return (
     <div className="flex items-center h-screen">
-        <div className="m-auto flex flex-col border-2 gap-3 border-black bg-red-300 p-10 rounded-2xl">
+        <div className="m-auto flex flex-col border-2 gap-3 border-black bg-blue-300 p-10 rounded-2xl">
             <CustomLabel label={"Пароль: "} />
             <CustomLabel label={"Логин: "} />
+
+             {/* TODO: Изучи как повесить событие на html компонент */}
+             
             <button className="border w-min m-auto px-5 py-1">Auth</button>
         </div>
     </div>
